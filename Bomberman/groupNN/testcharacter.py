b042512614235966a0e322292e9b238986dec6ac
Pedro Oporto <pdevasconcelloso@wpi.edu>
2019-02-26 01:46:39 +0000

    Edited check surroundings functions

--- Bomberman/groupNN/testcharacter.py
+++ Bomberman/groupNN/testcharacter.py
@@ -11,10 +11,11 @@ class TestCharacter(CharacterEntity):
     # type 1 is exit
     # type 2 is monster
     def check_surroundings(self, wrld, thisx, thisy):
-        # First check if exit is 1 move away
-        m = wrld.me(self)
+        # First check if exit is 1 move away\
+        finds = dict([(i,[]) for i in range(7)])
+        
         for dx in [-1, 0, 1]:
-            finds = []
+            
             x = thisx + dx
             
             # Avoid out-of-bound indexing
@@ -28,30 +29,26 @@ class TestCharacter(CharacterEntity):
                         if (y >=0) and (y < wrld.height()):
                             #Check cases
                             if (wlrd.empty_at(x,y)):
-                                finds.append((x,y,0))
+                                finds[0].append(dx,dy)
                             elif (wlrd.exit_at(x,y)):
-                                finds.append((x,y,1))
+                                finds[1].append(dx,dy)
                             elif (wlrd.wall_at(x,y)):
-                                finds.append((x,y,2))
+                                finds[2].append(dx,dy)
                             elif (wlrd.bomb_at(x,y)):
-                                finds.append((x,y,3))
+                                finds[3].append(dx,dy)
                             elif (wlrd.explosion_at(x,y)):
-                                finds.append((x,y,4))
+                                finds[4].append(dx,dy)
                             elif (wlrd.monsters_at(x,y)):
-                                finds.append((x,y,5))
+                                finds[5].append(dx,dy)
                             elif (wlrd.characters_at(x,y)):
-                                finds.append((x,y,6))
+                                finds[6].append(dx,dy)
         return finds;
     def do(self, wrld):
         # Your code here
         # First check if exit is 1 move away
-        for dx in [-1, 0, 1]:
-            # Avoid out-of-bound indexing
-            if (m.x+dx >=0) and (m.x+dx < wrld.width()):
-                # Loop through delta y
-                for dy in [-1, 0, 1]:
-                    # Make sure the monster is moving
-                    if (dx != 0) or (dy != 0):
-                        # Avoid out-of-bound indexing
-                        if (m.y+dy >=0) and (m.y+dy < wrld.height()):
+        me = wrld.me(self)
+        surroundings = check_surroundings(wlrd, me.x, me.y)
+        if len(surroundings[1]) > 0:
+            #return surroundings[1][0]
+        
         pass
